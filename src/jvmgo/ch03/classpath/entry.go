7ce1c7fbcd1eeecd3943f610ffba085cdf414633
李强进 <li.qj@ikcrm.com>
2019-04-09 18:38:36 +0800

    上传

---
+++ src/jvmgo/ch03/classpath/entry.go
@@ -0,0 +1,29 @@
+package classpath
+
+import "os"
+import "strings"
+
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	readClass(className string)([]byte,Entry,error)
+
+	String() string
+}
+
+func newEntry(path string) Entry {
+	if strings.Contains(path, pathListSeparator) {
+		return newCompositeEntry(path)
+	}
+
+	if (strings.HasSuffix(path, "*")) {
+		return newWildcardEntry(path)
+	}
+
+	if (strings.HasPrefix(path,".jar") || strings.HasPrefix(path,".JAR") || strings.HasPrefix(path,".zip") ||
+		strings.HasPrefix(path, ".ZIP")) {
+			return newZipEntry(path)
+	}
+
+	return newDirEntry(path)
+}
